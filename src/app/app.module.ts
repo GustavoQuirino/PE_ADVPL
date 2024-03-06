df79fc7d15f25a11f0dd7e4327cad5ebdb877bae
Gustavo Quirino <gh.quirino@yahoo.com>
2024-03-05 22:32:36 -0300

    ajust home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MT010MEMComponent } from './components/mt010-mem/mt010-mem.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, MT010MEMComponent],
+  declarations: [ 
+    AppComponent,
+    HomeComponent, 
+    MT010MEMComponent
+    ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
